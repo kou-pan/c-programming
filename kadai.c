46a576d291e79be363f64ffc5b77aa108d2c31f6
kou-pan <khs-090-vtc@e-net.nara.jp>
2025-05-16 06:20:41 +0000

    5-16

---
+++ kadai.c
@@ -0,0 +1,18 @@
+//長方形の面積を求める問題を作りなさい
+//2つの値を受け取り、その値を掛け算した値を返す関数
+#include <stdio.h>
+//関数のプロトタイプ宣言
+//main関数
+float chohokei(float tate,float yoko);
+int main(void){
+    float menseki = chohokei(5.0,3.0);//関数を呼び出すことができる
+    printf("%f\n",menseki);
+    return 0;
+}
+// chohokei関数
+float chohokei (float tate,float yoko/*引数*/){
+    //長方形の面積を求める内容→縦と横の掛け算
+    float menseki;
+    menseki=tate*yoko;
+    return menseki;
+}
